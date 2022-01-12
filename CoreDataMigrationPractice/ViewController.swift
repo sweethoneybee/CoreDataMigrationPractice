e3a6b07a57874650ce508a9871b79bf0584ea26e
sweethoneybee <jsjphone8@gmail.com>
2022-01-13 01:52:42 +0900

    project init

---
+++ CoreDataMigrationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataMigrationPractice
+//
+//  Created by 정성훈 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
